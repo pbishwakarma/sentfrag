a50699a4b381b86163cad61db99952744b1c46c1
Prajal Bishwakarma <pbishwakarma@email.wm.edu>
2021-06-01 10:41:58 -0400

    Change readers to just return raw text

--- sentfrag/preprocessors/readers.py
+++ sentfrag/preprocessors/readers.py
@@ -1,37 +1,16 @@
 from nltk.tokenize import sent_tokenize
 
 
-class Reader(object):
+class TXTReader():
 
     def __init__(self, filepath):
         self._filepath = filepath
-
-    def read(self, text):
-        """
-        Reads the provided text and returns a generator of strings. Each entry
-        contains a sentence.
-
-        Args:
-            text (str): The text from the infile.
-
-        Returns:
-            list: generator of sentences split by punctuation marks.
-        """
-        for sentence in sent_tokenize(text):
-            yield sentence.strip()
-
-
-class TXTReader(Reader):
-
-    def __init__(self, file):
-        super().__init__(file)
 
     def read(self):
         with open(self._filepath) as f:
             text = f.read()
 
-        return super().read(text)
-
+        return text
 
 SUPPORTED_READERS = {
     "txt": TXTReader
--- tests/preprocessors/test_readers.py
+++ tests/preprocessors/test_readers.py
@@ -3,7 +3,7 @@ import pytest
 from sentfrag.preprocessors.readers import TXTReader
 
 
-@pytest.mark.parametrize("file, expected", [("tests/data/test_document.txt", ["foo bar."])])
+@pytest.mark.parametrize("file, expected", [("tests/data/test_document.txt", "foo bar.")])
 def test_txt_reader_read(file, expected):
     reader = TXTReader(file)
 
